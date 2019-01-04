7bd9f0a8dd649ff4b1ae898e8ae75845a5a5acdb
A. Bram Neijt <bneijt@gmail.com>
2019-01-04 15:29:14 +0100

    Refactor for oembed

--- host.py
+++ host.py
@@ -1,20 +1,23 @@
 
 from flask import Flask
 import lfunction
+from flask import request
+import json
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render('')
+    return lfunction.lambda_editor({}, None)['body']
 
-@app.route("/<markdown>")
-def render(markdown):
-    return lfunction.lambda_handler({
-        'pathParameters': {
-            'markdown': markdown
-        }
-    }, None)['body']
+@app.route("/oembed")
+def render():
+    return json.dumps(lfunction.lambda_handler({
+        # 'pathParameters': {
+        #     'markdown': markdown
+        # },
+        'queryStringParameters': request.args
+    }, None)['body'])
 
 
 if __name__ == "__main__":
--- lfunction.py
+++ lfunction.py
@@ -2,35 +2,40 @@
 import base64
 import markdown
 import markdown.extensions.extra
+import urllib.parse as urlparse
 
-def lambda_handler(evt, context):
-    markdownSource = ''
-    try:
-        markdownSource = base64.urlsafe_b64decode(evt['pathParameters']['markdown']).decode("utf-8")
-    except Exception as e:
-        pass
-
-    if len(markdownSource) == 0:
-        return lambda_editor()
-
+def render_markdown(inputValue):
+    print(inputValue)
+    markdownSource = base64.urlsafe_b64decode(inputValue).decode("utf-8")
     markdownHtml = markdown.markdown(markdownSource, extensions=['extra'])
 
-    return {
-        'statusCode': 200,
-        'headers': {
-            "content-type": "text/html",
-            "Access-Control-Allow-Origin": "*"
-        },
-        'body': '''<!doctype html>
+    return '''<!doctype html>
 
         <html lang="en">
         <head>
           <meta charset="utf-8">
           <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/foundation/6.5.1/css/foundation.min.css">
         </head><body>''' + markdownHtml + '''</body></html>'''
+
+def lambda_handler(evt, context):
+    markdownInput = evt['queryStringParameters']['url']
+    return  {
+        'statusCode': 200,
+        'headers': {
+            "content-type": "application/json"
+        },
+        'body': {
+            "version": "1.0",
+            "url": evt['queryStringParameters']['url'],
+            "type": "rich",
+            "html": render_markdown(markdownInput),
+            "width": "500",
+            "height": "700"
+
+        }
     }
 
-def lambda_editor():
+def lambda_editor(evt, context):
     with open('index.html') as indexFile:
         return {
             'statusCode': 200,
